3b876852034772be320d46d9eb501ee068412435
Ernani Raffo <eraffo@ucsc.edu>
2022-05-10 21:41:55 -0700

    temp art

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,14 +1,34 @@
 class Play extends Phaser.Scene {
     constructor() {
-        super("PlayGame");
+        super("playGame");
     }
 
     preload() {
         // load assets here
+        this.load.spritesheet("slime", "./assets/slime.png", {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 1});
+        this.load.image("card", "./assets/card.png");
     }
 
     create() {
-        
+
+        // place enemy slime
+        this.slime = this.add.sprite(game.config.width / 2, game.config.height / 2, "slime");
+
+        // slime anim
+        this.anims.create({
+            key: "idle",
+            frames: this.anims.generateFrameNumbers("slime", {start: 0, end: 1}),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.slime.setScale(1.5);
+        this.slime.anims.play("idle");
+
+        // place card
+        this.card = this.add.sprite(game.config.width / 2, game.config.height - 23, "card");
+
+        this.card.setScale(1.5);
     }
 
     update() {
